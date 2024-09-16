a108d55257b109eb9807048f0fbcc74724af66aa
Texas <tanachote.la@gmail.com>
2024-09-16 21:16:50 +0700

    Finish

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fah) {
+  let convert = Math.round((Fah - 32) * (5/9)*10) / 10;
+  return convert
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Cel) {
+  let convert = Math.round((Cel * 9/5 + 32) * 10) / 10;
+  return convert
 };
 
 // Do not edit below this line
