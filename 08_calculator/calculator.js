aebb79ac5a001494cb3db513e45547395ed3fe7a
Texas <tanachote.la@gmail.com>
2024-09-30 19:01:57 +0700

    Done sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,14 +1,15 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
-};
+const sum = function(arr) {
+  let sum = arr.reduce((total, current) => total + current, 0);
+  return sum
+}
 
 const multiply = function() {
 
