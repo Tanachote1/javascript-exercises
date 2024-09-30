a935bb3fec0ed70525cd56b9f30a00c4432769e9
Texas <tanachote.la@gmail.com>
2024-09-30 23:32:58 +0700

    Finsihed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphaCharacter = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const newStr = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphaCharacter.includes(character))
+    .join('');
 
+    const reversedString = newStr.split('').reverse().join('');
+
+    return reversedString === newStr
 };
-
 // Do not edit below this line
 module.exports = palindromes;
